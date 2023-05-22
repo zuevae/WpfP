2372254c62909d39f60cba977346cd081a7301a6
ЗуеваЕЭ <@208-05>
2023-05-22 11:20:06 +0300

    Добавлены страницы добавления и редактирования(ошибок нет)

---
+++ WpfP/UpdAgent.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Path = System.IO.Path;
+
+namespace WpfP
+{
+    /// <summary>
+    /// Логика взаимодействия для UpdAgent.xaml
+    /// </summary>
+    public partial class UpdAgent : Page
+    {
+        public Frame frame1;
+        public UpdAgent()
+        {
+            InitializeComponent();
+        }
+    }
+}
